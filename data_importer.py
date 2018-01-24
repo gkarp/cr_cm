782f9ad40bfdc5c48507e9909c5ab2788cd75a36
gkarp <gkarp27@gmail.com>
2018-01-23 18:02:28 -0800

    Finalizing the scraper and creating a new file to use the scraper/send requests

---
+++ data_importer.py
@@ -0,0 +1,8 @@
+from sr_data_request import data_load
+
+'''
+This script will take as input the dictionary created by sr_data_request.py. It
+will then clean this data for the purposes of loading it into a database.
+'''
+
+
